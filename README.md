168cfe68a78c82eb1e4894e53083241f2b892d14
KrzysztofPa <krzysztofpawlak2000@gmail.com>
2021-07-01 11:35:56 +0200

    fix http(s) error

--- README.md
+++ README.md
@@ -1,12 +1,15 @@
+# LIVE https://krzysztofpa.github.io/Weather-App/
+
 # Weather App
-Weather App is an application that uses OpenWeatherMap to check the current weather in any city in the world, 
+
+Weather App is an application that uses OpenWeatherMap to check the current weather in any city in the world,
 
 my first project using api
 
 ## Contributing
+
 Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
 
 ## License
+
 [MIT](https://choosealicense.com/licenses/mit/)
-
-
--- script.js
+++ script.js
@@ -1,48 +1,48 @@
-const inputCityName = document.querySelector('input');
-const sendBtn = document.querySelector('button');
+const inputCityName = document.querySelector("input");
+const sendBtn = document.querySelector("button");
 
-const cityName = document.querySelector('.city-name');
-const warning = document.querySelector('.warning');
-const photo = document.querySelector('.photo');
+const cityName = document.querySelector(".city-name");
+const warning = document.querySelector(".warning");
+const photo = document.querySelector(".photo");
 
-const weather = document.querySelector('.weather');
-const temperature = document.querySelector('.temp');
-const humidity = document.querySelector('.humidity');
-const pressure = document.querySelector('.pressure');
-const windSpeed = document.querySelector('.windSpeed')
-const windDirection = document.querySelector('.windDirection')
+const weather = document.querySelector(".weather");
+const temperature = document.querySelector(".temp");
+const humidity = document.querySelector(".humidity");
+const pressure = document.querySelector(".pressure");
+const windSpeed = document.querySelector(".windSpeed");
+const windDirection = document.querySelector(".windDirection");
 
+inputCityName.value = "Warszawa";
+connectAndFill();
 
+sendBtn.addEventListener("click", connectAndFill);
 
-
-inputCityName.value = 'Warszawa';
-connectAndFill()
-
-
-sendBtn.addEventListener('click', connectAndFill);
-
-inputCityName.addEventListener('keypress', (e) => {
-    if (e.key === "Enter") {
-        connectAndFill()
-    }
-})
+inputCityName.addEventListener("keypress", (e) => {
+  if (e.key === "Enter") {
+    connectAndFill();
+  }
+});
 
 function connectAndFill() {
-    const apiReadyLink = `http://api.openweathermap.org/data/2.5/weather?q=${inputCityName.value}&appid=82cd48d88651a90dc6c5a37de63c5a7d&units=metric`;
-    axios.get(apiReadyLink)
-        .then((res) => {
-            windDirection.style.transform = `rotate(${res.data.wind.deg}deg)`
-            warning.innerText = '';
-            windSpeed.innerText = res.data.wind.speed + 'm/s'
-            pressure.innerText = res.data.main.pressure + 'Hpa';
-            weather.innerText = res.data.weather[0].main;
-            cityName.innerText = res.data.name;
-            temperature.innerText = res.data.main.temp.toFixed(0) + '°C';
-            humidity.innerText = `${res.data.main.humidity}%`;
-            photo.setAttribute('src', `http://openweathermap.org/img/w/${res.data.weather[0].icon}.png`)
-            inputCityName.value = '';
-        })
-        .catch((err) => {
-            warning.innerText = 'Please, enter the correct city name';
-        })
+  const apiReadyLink = `https://api.openweathermap.org/data/2.5/weather?q=${inputCityName.value}&appid=82cd48d88651a90dc6c5a37de63c5a7d&units=metric`;
+  axios
+    .get(apiReadyLink)
+    .then((res) => {
+      windDirection.style.transform = `rotate(${res.data.wind.deg}deg)`;
+      warning.innerText = "";
+      windSpeed.innerText = res.data.wind.speed + "m/s";
+      pressure.innerText = res.data.main.pressure + "Hpa";
+      weather.innerText = res.data.weather[0].main;
+      cityName.innerText = res.data.name;
+      temperature.innerText = res.data.main.temp.toFixed(0) + "°C";
+      humidity.innerText = `${res.data.main.humidity}%`;
+      photo.setAttribute(
+        "src",
+        `https://openweathermap.org/img/w/${res.data.weather[0].icon}.png`
+      );
+      inputCityName.value = "";
+    })
+    .catch((err) => {
+      warning.innerText = "Please, enter the correct city name";
+    });
 }
